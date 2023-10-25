0937b32fe94c0ce2b36f4fb8f84b1661f24c28a9
raphaelBureau <raphael.burea@gmail.com>
2023-10-25 17:43:00 -0400

    gameManager code refactoring

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -59,56 +59,6 @@ public class GameManager : MonoBehaviour
 
     string player1Name = "Joueur 1";
     string player2Name = "Joueur 2";
-    public void EnableMultiplayer(bool first)
-    {
-        multiplayer = true;
-        sendPackets = first;
-        CM.Active = !first;
-        UpdateProfiles();
-    }
-
-    public void OtherPlayerPlayRequest() //resume la partie, assume que chaque balle a cessé de bouger
-    {
-        CM.Active = false;
-        sendPackets = true;
-        playerTurn = !playerTurn;
-        UpdateProfiles();
-        if (mainBallFell)
-        {
-            PlaceMainBall();
-        }
-    }
-
-    public void UpdateBalls(BallData ball)
-    {
-        print("update ball id:" + ball.id + " active: " + ball.active + " collisions: " + ball.collisions);
-        if (ball.id == 0) //main ball
-        {
-            if (ball.collisions) //si collisions, la balle est placée, sinon, elle est en placement du joueur
-            {
-                mainBall.SetActive(ball.active);
-                mainBall.GetComponent<Collider>().isTrigger = false;
-                mainBall.GetComponent<Collider>().enabled = true;
-                mainBall.GetComponent<Rigidbody>().isKinematic = false;
-            }
-            else
-            {
-                mainBall.SetActive(ball.active);
-                mainBall.GetComponent<Collider>().isTrigger = true;
-                mainBall.GetComponent<Collider>().enabled = true;
-                mainBall.GetComponent<Rigidbody>().isKinematic = true;
-            }
-            if(!ball.active) //si le gameobject est disabled la balle est tombe dans un trou donc le joueur doit la placer au prochain tour
-            {
-                mainBallFell = true;
-            }
-        }
-        else
-        {//balle normalle 
-            ballList[ball.id].SetActive(ball.active);
-            BallFell(ball.id, ball.id <= 8);
-        }
-    }
 
     bool placingMainBall = false;
     bool mainBallFell = false;
@@ -157,36 +107,7 @@ public class GameManager : MonoBehaviour
     {
         if (placingMainBall && sendPackets)
         {
-            net.SendBallData(new BallData(0, true, false)); //
-            print("raycasting moment");
-            RaycastHit hit;
-            Ray ray = CM.TopCam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, 50f, LayerMask.GetMask("Surface")))
-            {
-                mainBall.transform.position = new Vector3(hit.point.x, 1.3f, hit.point.z);
-                mainBall.GetComponent<Renderer>().material.color = new Color(1f, 0.5f, 0.5f);
-              //  print(mainBall.GetComponent<mainBall>().GoodPlacement);
-                if (mainBall.GetComponent<mainBall>().GoodPlacement)
-                {
-                    mainBall.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
-                    if (Input.GetMouseButton(0))
-                    {
-
-                        mainBall.layer = 8;
-                        mainBall.GetComponent<Collider>().isTrigger = false;
-                        mainBall.GetComponent<Rigidbody>().isKinematic = false;
-                        CM.UnlockTop();
-                        placingMainBall = false;
-
-                        Net.UpdateAllBalls();
-                        net.SendBallData(new BallData(0, true, true));
-                        mainBall.GetComponent<mainBall>().placing = false;
-                        mainBallFell = false;
-                        mainBallNoReplay = false;
-
-                    }
-                }
-            }
+            PlaceMainBall();
         }
         else
         {
@@ -195,68 +116,17 @@ public class GameManager : MonoBehaviour
             {
                 if (playing)
                 { //donne le controlle de la camera au user et permet de frapper la boulle
-                    mouseClick.left = Input.GetMouseButton(0);
-                    mouseClick.right = Input.GetMouseButton(1);
+                    bool changed = HandleInput();
 
-                    mouseMove = Input.mousePosition;
-                    if (mouseClick.right)
-                    {//user controls
-                     //y rotation
-                        float yR = (mouseMove.x - prevMouseMove.x) / screen.width * 100;
-                        CM.Cam.transform.RotateAround(mainBall.transform.position, CM.Cam.transform.up, yR);
-
-                        //x rotation
-                        float xR = (mouseMove.y - prevMouseMove.y) / screen.height * 100;
-                        CM.Cam.transform.RotateAround(mainBall.transform.position, CM.Cam.transform.right, -xR);
-
-
-                        CM.Cam.transform.LookAt(mainBall.transform.position);
+                    if (changed)
+                    {
+                        UpdatePlayControls();
                     }
-
-
-                    hitStrenght += Input.mouseScrollDelta.y;
-                    trajectory.transform.position = new Vector3(mainBall.transform.position.x, 0.6f, mainBall.transform.position.z) + new Vector3(CM.Cam.transform.forward.x, 0, CM.Cam.transform.forward.z).normalized * hitStrenght / 2;
-                    trajectory.transform.localScale = new Vector3(0.02f, 1, hitStrenght / 10f);
-                    trajectory.transform.LookAt(new Vector3(mainBall.transform.position.x, 0.6f, mainBall.transform.position.z) + new Vector3(CM.Cam.transform.forward.x, 0, CM.Cam.transform.forward.z).normalized * hitStrenght);
-                    trajectory.GetComponent<MeshRenderer>().material.mainTextureScale = new Vector2(1, hitStrenght / 2);
-
-                    if (mouseClick.left && !CM.InUI)
-                    {//firrre!!!
-                        mainBall.GetComponent<Rigidbody>().velocity = new Vector3(CM.Cam.transform.forward.x, 0, CM.Cam.transform.forward.z).normalized * hitStrenght;
-                        trajectory.SetActive(false);
-
-                        playAgain = false;
-                        playing = false;
-                        CM.Active = true;
-                        Net.UpdateAllBalls();
-                    }
-
-                    prevMouseClick = mouseClick;
-                    prevMouseMove = mouseMove;
 
                 }
                 else //deplacer la camera vers la ballle
                 {
-                    var targetRotation = Quaternion.LookRotation(mainBall.transform.position - CM.Cam.transform.position);
-                    if (Mathf.Abs((CM.Cam.transform.position - mainBall.transform.position + Vector3.up * 2).magnitude) <= 7.1 && (Mathf.Abs(CM.Cam.transform.rotation.w - targetRotation.w + CM.Cam.transform.rotation.x - targetRotation.x) < 5))
-                    {
-                        playing = true;
-                        trajectory.SetActive(true);
-                        trajectory.transform.position = new Vector3(mainBall.transform.position.x, 0.6f, mainBall.transform.position.z) + new Vector3(CM.Cam.transform.forward.x, 0, CM.Cam.transform.forward.z).normalized * hitStrenght / 2;
-                        trajectory.transform.localScale = new Vector3(0.02f, 1, hitStrenght / 10f);
-                        trajectory.transform.LookAt(new Vector3(mainBall.transform.position.x, 0.6f, mainBall.transform.position.z) + new Vector3(CM.Cam.transform.forward.x, 0, CM.Cam.transform.forward.z).normalized * hitStrenght);
-                        trajectory.GetComponent<MeshRenderer>().material.mainTextureScale = new Vector2(1, hitStrenght / 2);
-                    }
-                    else
-                    {
-                        if (Mathf.Abs((CM.Cam.transform.position - mainBall.transform.position + Vector3.up * 2).magnitude) > 7)
-                        {
-                            CM.Cam.transform.position = Vector3.MoveTowards(CM.Cam.transform.position, mainBall.transform.position + Vector3.up * 2, 10 * Time.deltaTime);
-                        }
-
-
-                        CM.Cam.transform.rotation = Quaternion.Slerp(CM.Cam.transform.rotation, targetRotation, 3 * Time.deltaTime);
-                    }
+                    MoveCamToBall();
                 }
             }
         }
@@ -290,7 +160,126 @@ public class GameManager : MonoBehaviour
             }
         }
     }
+    bool HandleInput()
+    {
+        bool changed = false;
+
+        mouseClick.left = Input.GetMouseButton(0);
+        mouseClick.right = Input.GetMouseButton(1);
+
+        mouseMove = Input.mousePosition;
+        if (mouseClick.right)
+        {//user controls
+         //y rotation
+            float yR = (mouseMove.x - prevMouseMove.x) / screen.width * 100;
+            CM.Cam.transform.RotateAround(mainBall.transform.position, CM.Cam.transform.up, yR);
+
+            //x rotation
+            float xR = (mouseMove.y - prevMouseMove.y) / screen.height * 100;
+            CM.Cam.transform.RotateAround(mainBall.transform.position, CM.Cam.transform.right, -xR);
+
+
+            CM.Cam.transform.LookAt(mainBall.transform.position);
+
+            if (mouseMove.x != prevMouseMove.x || mouseMove.y != prevMouseMove.y)
+            {
+                changed = true;
+            }
+        }
+
+        if (Mathf.Abs(Input.mouseScrollDelta.y) > 0)
+        {
+            hitStrenght += Input.mouseScrollDelta.y;
+            changed = true;
+        }
+        if(prevMouseClick.left != mouseClick.left || prevMouseClick.right != mouseClick.right)
+        {
+            changed = true;
+        }
+
+        prevMouseClick = mouseClick;
+        prevMouseMove = mouseMove;
+
+        return changed;
+    }
+    void UpdatePlayControls()
+    {
+        Vector3 ball = mainBall.transform.position;
+        Vector3 camf = CM.Cam.transform.forward;
+        trajectory.transform.position = new Vector3(ball.x, 0.6f, ball.z) + new Vector3(camf.x, 0, camf.z).normalized * hitStrenght / 2;
+        trajectory.transform.localScale = new Vector3(0.02f, 1, hitStrenght / 10f);
+        trajectory.transform.LookAt(new Vector3(ball.x, 0.6f, ball.z) + new Vector3(camf.x, 0, camf.z).normalized * hitStrenght);
+        trajectory.GetComponent<MeshRenderer>().material.mainTextureScale = new Vector2(1, hitStrenght / 2);
+
+        if (mouseClick.left && !CM.InUI)
+        {//firrre!!!
+            mainBall.GetComponent<Rigidbody>().velocity = new Vector3(CM.Cam.transform.forward.x, 0, CM.Cam.transform.forward.z).normalized * hitStrenght;
+            trajectory.SetActive(false);
+
+            playAgain = false;
+            playing = false;
+            CM.Active = true;
+            Net.UpdateAllBalls();
+        }
+    }
     void PlaceMainBall()
+    {
+        net.SendBallData(new BallData(0, true, false)); //
+                                                        // print("raycasting moment");
+        RaycastHit hit;
+        Ray ray = CM.TopCam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 50f, LayerMask.GetMask("Surface")))
+        {
+            mainBall.transform.position = new Vector3(hit.point.x, 1.3f, hit.point.z);
+            mainBall.GetComponent<Renderer>().material.color = new Color(1f, 0.5f, 0.5f);
+            //  print(mainBall.GetComponent<mainBall>().GoodPlacement);
+            if (mainBall.GetComponent<mainBall>().GoodPlacement)
+            {
+                mainBall.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+                if (Input.GetMouseButton(0))
+                {
+
+                    mainBall.layer = 8;
+                    mainBall.GetComponent<Collider>().isTrigger = false;
+                    mainBall.GetComponent<Rigidbody>().isKinematic = false;
+                    CM.UnlockTop();
+                    placingMainBall = false;
+
+                    Net.UpdateAllBalls();
+                    net.SendBallData(new BallData(0, true, true));
+                    mainBall.GetComponent<mainBall>().placing = false;
+                    mainBallFell = false;
+                    mainBallNoReplay = false;
+
+                }
+            }
+        }
+    }
+    void MoveCamToBall()
+    {
+        Transform camTran = CM.Cam.transform;
+        Vector3 ballPos = mainBall.transform.position;
+
+        Quaternion targetRotation = Quaternion.LookRotation(ballPos - camTran.position);
+        if (Mathf.Abs((camTran.position - mainBall.transform.position + Vector3.up * 2).magnitude) > 7)
+        {
+            camTran.position = Vector3.MoveTowards(camTran.position, ballPos + Vector3.up * 2, 10 * Time.deltaTime);
+        }
+
+        camTran.rotation = Quaternion.Slerp(camTran.rotation, targetRotation, 3 * Time.deltaTime);
+
+        if (Mathf.Abs((camTran.position - ballPos + Vector3.up * 2).magnitude) <= 7.1 && (Mathf.Abs(camTran.rotation.w - targetRotation.w + camTran.rotation.x - targetRotation.x) < 5))
+        {
+            playing = true;
+            trajectory.SetActive(true);
+            trajectory.transform.position = new Vector3(ballPos.x, 0.6f, ballPos.z) + new Vector3(camTran.forward.x, 0, camTran.forward.z).normalized * hitStrenght / 2;
+            trajectory.transform.localScale = new Vector3(0.02f, 1, hitStrenght / 10f);
+            trajectory.transform.LookAt(new Vector3(ballPos.x, 0.6f, ballPos.z) + new Vector3(camTran.forward.x, 0, camTran.forward.z).normalized * hitStrenght);
+            trajectory.GetComponent<MeshRenderer>().material.mainTextureScale = new Vector2(1, hitStrenght / 2);
+        }
+
+    }
+    void MainBallPlacementRequest()
     {
         CM.LockTop();
         placingMainBall = true;
@@ -306,7 +295,6 @@ public class GameManager : MonoBehaviour
             net.SendBallData(new BallData(0, true, false));
         }
     }
-
     public void BallFell(int ballNo,bool solidColor)
     {
             if(ballNo == 8)
@@ -405,7 +393,54 @@ public class GameManager : MonoBehaviour
             }
         UpdateProfiles();
     }
-
+    public void UpdateBalls(BallData ball)
+    {
+        //print("update ball id:" + ball.id + " active: " + ball.active + " collisions: " + ball.collisions);
+        if (ball.id == 0) //main ball
+        {
+            if (ball.collisions) //si collisions, la balle est placée, sinon, elle est en placement du joueur
+            {
+                mainBall.SetActive(ball.active);
+                mainBall.GetComponent<Collider>().isTrigger = false;
+                mainBall.GetComponent<Collider>().enabled = true;
+                mainBall.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            else
+            {
+                mainBall.SetActive(ball.active);
+                mainBall.GetComponent<Collider>().isTrigger = true;
+                mainBall.GetComponent<Collider>().enabled = true;
+                mainBall.GetComponent<Rigidbody>().isKinematic = true;
+            }
+            if (!ball.active) //si le gameobject est disabled la balle est tombe dans un trou donc le joueur doit la placer au prochain tour
+            {
+                mainBallFell = true;
+            }
+        }
+        else
+        {//balle normalle 
+            ballList[ball.id].SetActive(ball.active);
+            BallFell(ball.id, ball.id <= 8);
+        }
+    }
+    public void EnableMultiplayer(bool first)
+    {
+        multiplayer = true;
+        sendPackets = first;
+        CM.Active = !first;
+        UpdateProfiles();
+    }
+    public void OtherPlayerPlayRequest() //resume la partie, assume que chaque balle a cessé de bouger
+    {
+        CM.Active = false;
+        sendPackets = true;
+        playerTurn = !playerTurn;
+        UpdateProfiles();
+        if (mainBallFell)
+        {
+            MainBallPlacementRequest();
+        }
+    }
     void UpdateProfiles()
     {
         if (!playerTurn)
